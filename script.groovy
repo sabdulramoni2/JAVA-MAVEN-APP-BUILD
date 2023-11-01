f1665f04bbb7b404c627f22fdfae9a8005caa054
sabdulramoni <abdulramonisaidi@yahoo.com>
2023-10-31 21:59:45 -0400

    update image tag to 3.0

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ def buildJar() {
 def buildImage() {
             echo "building the docker image....."
             withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-                    sh 'docker build -t ashiwaju/jupiter12:jma-2.0 .'
+                    sh 'docker build -t ashiwaju/jupiter12:jma-3.0 .'
                      sh 'echo $PASS | docker login -u $USER --password-stdin'   
-                     sh 'docker push ashiwaju/jupiter12:jma-2.0'
+                     sh 'docker push ashiwaju/jupiter12:jma-3.0'
             }
 }
 
