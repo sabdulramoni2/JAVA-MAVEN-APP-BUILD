4fa1595a5c9908454e372afb8794311463da52b1
saidi abdulramoni <144086740+sabdulramoni2@users.noreply.github.com>
2023-11-02 00:27:54 -0400

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,4 +1,17 @@
+def buildJar() {
+    echo 'building the application..'
+    sh 'mvn package'
+}
 
+
+def buildImage() {
+            echo "building the docker image....."
+            withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+                    sh 'docker build -t ashiwaju/jupiter12:jma-3.0 .'
+                     sh 'echo $PASS | docker login -u $USER --password-stdin'   
+                     sh 'docker push ashiwaju/jupiter12:jma-3.0'
+            }
+}
 
 def testApp() {
     echo 'testing the application..'
